1cbde0754b409a0d8511d5189cd1cf1a03d174db
Flavien Marck <marck12u@sd-008-12.iutsd.site.univ-lorraine.fr>
2023-09-15 09:58:51 +0200

    Initialisation

---
+++ 2048/2048/_048App.swift
@@ -0,0 +1,17 @@
+//
+//  _048App.swift
+//  2048
+//
+//  Created by Flavien Marck on 15/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct _048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
